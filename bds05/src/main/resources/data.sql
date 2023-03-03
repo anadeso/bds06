3af0014189d893eecab06520b03649406f8debbe
anadeso <anadeso90@gmail.com>
2023-03-03 08:51:26 -0300

    Criando DTO requisicao genres

--- bds05/src/main/resources/data.sql
+++ bds05/src/main/resources/data.sql
@@ -8,7 +8,12 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 
-INSERT INTO tb_genre (name) VALUES ('Maria');
+INSERT INTO tb_genre (name) VALUES ('Ação e aventura');
+INSERT INTO tb_genre (name) VALUES ('Drama');
+INSERT INTO tb_genre (name) VALUES ('Comédia romântica');
+INSERT INTO tb_genre (name) VALUES ('Ficção científica');
+INSERT INTO tb_genre (name) VALUES ('Terror');
+
 INSERT INTO tb_movie (title, sub_Title, year, img_Url, synopsis, genre_id) VALUES ('Title', 'subTitle', 2021, 'https://imgurl.com/img.png', 'Synopsis', 1);
 INSERT INTO tb_review (text, user_id, movie_id) VALUES ('Text', 1, 1);
 
